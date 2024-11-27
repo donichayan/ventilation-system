12dc6f0505458ae0e2775a9c2707ce68a9228436
donichayan <don@turquoic.com>
2024-11-27 07:04:42 +0530

    initial commit

---
+++ main.c
@@ -0,0 +1,16 @@
+/*
+ * main.c
+ *
+ * Created: 11/27/2024 6:55:02 AM
+ *  Author: liali
+ */ 
+
+#include <xc.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
